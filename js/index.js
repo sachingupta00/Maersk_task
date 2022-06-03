11b519a226d65dd7ed4dd0240efa70a76260a5bc
3718324 <3718324@fedex.com>
2022-06-03 14:46:38 +0530

    updated with fix for shuffle

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     const colors = { '1':'#000000',
         '2':'#2B8EAD',
         '3':'#333333', '4':'#6F98A8',
-        '5':'#FFFFFF', '6':'#BFBFBF',
-        '7':'#EFEFEF', '8':'#2F454E',
+        '5':'#6c757d', '6':'#BFBFBF',
+        '7':'#6c757d', '8':'#2F454E',
         '9':'#72C3DC'};
     var x = Object.keys(colors);
     let rowHt = (num,i)=>{
-        return '<div class="col-3 grid c'+num+'"><div class="num">'+num+'</div></div>'+(i==3 ? '<div class="col-3 grid">\
+        return '<div class="col-3 grid c'+num+' text-center font-weight-bold"><div class="num">'+num+'</div></div>'+(i==3 ? '<div class="col-3 grid">\
             <button id="shuf" class="btn btn-primary m-2">SHUFFLE</button>\
             <button id="sort" class="btn btn-primary m-2">SORT</button>\
         </div>':'');
@@ -21,13 +21,13 @@ $(document).ready(function(){
     createGrid = (x)=>{
         let html='',ss = 960,  /* screen size */
         sh = window.innerHeight-30, /* screen height */
-        css = 'body{background-color:#f8f9fa;}.grid{width:'+(ss/4)+'px;height:'+(sh/3)+'px;color:white;}'; /* css variable  */
+        css = 'body{background-color:#f8f9fa;}.grid{font-size: -webkit-xxx-large;width:'+(ss/4)+'px;height:'+(sh/3)+'px;color:white;}'; /* css variable  */
         
-        for(let i=1;i<9;i++){
-            html += (x[i-1]%3==1) ? '<div class="row">':'';
+        for(let i=1;i<=9;i++){
+            html += (i%3==1) ? '<div class="row">':'';
             css += '.c'+i+'{ background-color:'+colors[(x[i-1]).toString()]+'}';
             html += rowHt(x[i-1],i);
-            html += (x[i-1]%3==0) ? '</div>':'';
+            html += (i%3==0) ? '</div>':'';
         }
         $('#root').html(html);
         $('head style').remove();
